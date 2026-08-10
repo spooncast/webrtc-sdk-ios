85334cf256e4021f00b8f50f6604b3edc2ffacce
ben-spoonradio <97279447+ben-spoonradio@users.noreply.github.com>
2026-08-10 14:19:03 +0900

    Update Package.swift to 146.7680.01 (M146, no dSYM)

--- Package.swift
+++ Package.swift
@@ -6,24 +6,17 @@ import PackageDescription
 let package = Package(
     name: "WebRTC",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "WebRTC",
             targets: ["WebRTC"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "WebRTC",
-            url: "https://github.com/spooncast/webrtc-sdk-ios/releases/download/104.5112.01/WebRTC.xcframework.zip",
-            checksum: "e582f5360471515bd4cfbe941a5315809f478984f2ce2ed8d67ab40e04a989e7"
+            url: "https://github.com/spooncast/webrtc-sdk-ios/releases/download/146.7680.01/WebRTC.xcframework.zip",
+            checksum: "74b6156386d17c6cf833cf032e4635ee984dac1b23abc40c717757cfd9637adc"
         ),
-
-
     ]
 )
